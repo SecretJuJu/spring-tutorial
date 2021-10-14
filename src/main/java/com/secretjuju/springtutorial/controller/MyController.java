d90911319f6b8f50d1062b03246f000336d13e53
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-14 13:01:23 +0900

    add: msg를 바꿀 수 있는 updateMsg 메서드 작성

--- src/main/java/com/secretjuju/springtutorial/controller/MyController.java
+++ src/main/java/com/secretjuju/springtutorial/controller/MyController.java
@@ -1,15 +1,30 @@
 package com.secretjuju.springtutorial.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @Controller
 public class MyController {
     String message = "helloworld";
 
     @GetMapping("/msg")
-    public String sayMsg() {
+    public @ResponseBody String sayMsg() {
         return this.message;
     }
 
+    @PutMapping("/msg")
+    public ResponseEntity<?> updateMsg(
+            @RequestBody HashMap<String, String> updateMsgForm) {
+        String message = updateMsgForm.get("msg");
+        if (message.length() < 100) {
+            this.message = message;
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 }
